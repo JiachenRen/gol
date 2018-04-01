72f1c1a13bf4ffbf3c4750fd263edcb5779ecf87
Jiachen Ren <arc4jiachenren@gmail.com>
2018-04-01 13:27:59 -0400

    Remove unnecessary imports

--- src/ConvertToStandardConfig.java
+++ src/ConvertToStandardConfig.java
@@ -1,11 +1,5 @@
 import jui.TextInput;
 
-//import java.io.FileNotFoundException;
-//import java.io.PrintWriter;
-//import java.io.UnsupportedEncodingException;
-//import java.net.URISyntaxException;
-//import java.util.Scanner;
-
 /**
  * Created by Jiachen on 6/14/17.
  * converts a #R file to standard #config file in Jiachen's Game of Life implementation
